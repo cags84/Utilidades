03da20ee6208992f5450602f754e5b67db8abbd6
Carlos Guzman <cags84@gmail.com>
2015-05-26 10:45:24 -0500

    Iptables
    Script que permite la configuración de un servidor Linux en un router
    con funciones de proxy

---
+++ Iptables/start.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+# Ruta de Iptables
+IPT="/sbin/iptables"
+# Lista de ip a bloquear
+SPAMLIST="blockedip"
+# Mensaje devuelto en el LOG
+SPAMDROPMSG="BLOCKED IP DROP"
+# Interface conectada a la WAN
+INTERNET="eth1"
+# IP WAN
+IP_WAN="190.90.38.187"
+# Interface conectada a la LAN
+LAN_IN="eth0"
+# IP del servidor donde escucha
+SQUID_SERVER="10.116.80.103"
+# Puerto de Squid
+SQUID_PORT="3128"
+ 
+echo "Arranco firewall, script de configuracion ....."
+$IPT -F
+$IPT -X
+$IPT -t nat -F
+$IPT -t nat -X
+$IPT -t mangle -F
+$IPT -t mangle -X
+
+# Cargamos los modulos
+modprobe ip_conntrack
+modprobe ip_conntrack_ftp
+ 
+[ -f /root/scripts/blocked.ips.txt ] && BADIPS=$(egrep -v -E "^#|^$" /root/scripts/blocked.ips.txt)
+ 
+PUB_IF="eth1"
+ 
+# Permitimos el acceso ilimitado a la maquina local
+$IPT -A INPUT -i lo -j ACCEPT
+$IPT -A OUTPUT -o lo -j ACCEPT
+ 
+# hacemos DROP a todo el trafico por defecto
+$IPT -P INPUT DROP
+$IPT -P OUTPUT DROP
+$IPT -P FORWARD DROP
+ 
+if [ -f /root/scripts/blocked.ips.txt ];
+then
+# Creamos una nueva lista con iptables
+$IPT -N $SPAMLIST
+ 
+for ipblock in $BADIPS
+do
+   $IPT -A $SPAMLIST -s $ipblock -j LOG --log-prefix "$SPAMDROPMSG"
+   $IPT -A $SPAMLIST -s $ipblock -j DROP
+done
+ 
+$IPT -I INPUT -j $SPAMLIST
+$IPT -I OUTPUT -j $SPAMLIST
+$IPT -I FORWARD -j $SPAMLIST
+fi
+ 
+# Block sync
+$IPT -A INPUT -i ${PUB_IF} -p tcp ! --syn -m state --state NEW  -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "Drop Sync"
+$IPT -A INPUT -i ${PUB_IF} -p tcp ! --syn -m state --state NEW -j DROP
+ 
+# Block Fragments
+$IPT -A INPUT -i ${PUB_IF} -f  -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "Fragments Packets"
+$IPT -A INPUT -i ${PUB_IF} -f -j DROP
+ 
+# Block bad stuff
+$IPT  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL FIN,URG,PSH -j DROP
+$IPT  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL ALL -j DROP
+ 
+$IPT  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL NONE -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "NULL Packets"
+$IPT  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL NONE -j DROP # NULL packets
+ 
+$IPT  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+ 
+$IPT  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags SYN,FIN SYN,FIN -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "XMAS Packets"
+$IPT  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP #XMAS
+ 
+$IPT  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags FIN,ACK FIN -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "Fin Packets Scan"
+$IPT  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags FIN,ACK FIN -j DROP # FIN packet scans
+ 
+$IPT  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
+ 
+# Allow full outgoing connection but no incomming stuff
+$IPT -A INPUT -i eth1 -m state --state ESTABLISHED,RELATED -j ACCEPT
+$IPT -A OUTPUT -o eth1 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+ 
+# Allow ssh
+$IPT -A INPUT -p tcp --destination-port 22 -j ACCEPT
+ 
+# allow incomming ICMP ping pong stuff
+$IPT -A INPUT -p icmp --icmp-type 8 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+$IPT -A OUTPUT -p icmp --icmp-type 0 -m state --state ESTABLISHED,RELATED -j ACCEPT
+ 
+# Allow port 53 tcp/udp (DNS Server)
+$IPT -A INPUT -p udp --dport 53 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+$IPT -A OUTPUT -p udp --sport 53 -m state --state ESTABLISHED,RELATED -j ACCEPT
+ 
+$IPT -A INPUT -p tcp --destination-port 53 -m state --state NEW,ESTABLISHED,RELATED  -j ACCEPT
+$IPT -A OUTPUT -p tcp --sport 53 -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# Permitimos el puerto 3128 tcp (Servidor Proxy -> Squid)
+$IPT -A INPUT -p tcp --destination-port 3128 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+$IPT -A OUTPUT -p tcp --sport 53 -m state --state ESTABLISHED,RELATED -j ACCEPT
+ 
+# Abrimos el puerto 80 - Servidor WEB
+$IPT -A INPUT -p tcp --destination-port 80 -j ACCEPT
+
+##### Agregamos aqui las reglas propias ######
+ 
+echo "1" > /proc/sys/net/ipv4/ip_forward 
+
+# set this system as a router for Rest of LAN
+iptables --table nat --append POSTROUTING --out-interface $INTERNET -j MASQUERADE
+iptables --append FORWARD --in-interface $LAN_IN -j ACCEPT
+
+# DNAT port 80 request comming from LAN systems to squid 3128 ($SQUID_PORT) aka transparent proxy
+iptables -t nat -A PREROUTING -i $LAN_IN -p tcp --dport 80 -j DNAT --to $SQUID_SERVER:$SQUID_PORT
+
+# if it is same system
+iptables -t nat -A PREROUTING -i $INTERNET -p tcp --dport 80 -j REDIRECT --to-port $SQUID_PORT
+ 
+##### Fin Reglas Propias ############
+ 
+# Esto se hace para lo hacer log de smb/windows sharing packets - Es mucho log y ocupa mucho espacio
+$IPT -A INPUT -p tcp -i eth0 --dport 137:139 -j REJECT
+$IPT -A INPUT -p udp -i eth0 --dport 137:139 -j REJECT
+ 
+# log everything else and drop
+$IPT -A INPUT -j LOG
+$IPT -A FORWARD -j LOG
+$IPT -A INPUT -j DROP
+ 
+exit 0
